941a2a7c8630c3552040f6b77c7dc0492be82e55
Sanctorum Thomas <sanctorum.thomas@gmail.com>
2018-07-01 01:05:43 +0200

    fix encoding, add array of unique word for future transation

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const app = require('express')();
-const bodyParser = require('body-parser');
 const Scrapper = require('./lib/kh-scrapper');
 const fs = require('fs');
 const kh = new Scrapper();
@@ -7,7 +6,7 @@ const kh = new Scrapper();
 app.listen(3000, () => {
     kh.init()
         .then(data => {
-            fs.writeFileSync('./output/mixage.json', JSON.stringify(data), 'utf-8');
+            fs.writeFileSync('./output/mixage_fr.json', JSON.stringify(data), 'utf-8');
             console.log('output file in ./output/');
             process.exit(0);
         });
